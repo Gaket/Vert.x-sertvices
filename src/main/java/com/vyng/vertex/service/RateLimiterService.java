f49ced42b6fd7c3c16545d7cc4ce93b83f020f26
rajat gupta <rajatgupta@rajats-MacBook-Pro.local>
2019-12-11 16:57:06 +0530

    api for getting  video by phoneNumber

---
+++ src/main/java/com/vyng/vertex/service/RateLimiterService.java
@@ -0,0 +1,36 @@
+package com.vyng.vertex.service;
+
+import com.mongodb.lang.NonNull;
+import com.vyng.vertex.error.QueryLimitReachedException;
+import com.vyng.vertex.utils.Utils;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.redis.RedisClient;
+
+import java.util.concurrent.TimeUnit;
+
+public class RateLimiterService {
+    private static final int LIMITATION_TIME = (int) TimeUnit.HOURS.toSeconds(12);
+
+    @NonNull
+    private final RedisClient redisClient;
+
+    public RateLimiterService(@NonNull RedisClient redisClient) {
+        this.redisClient = redisClient;
+    }
+
+    public Future<Boolean> getTotalRequestLimitFuture(String key, int maxRequests, String error) {
+        Promise<Long> incrementedCount = Promise.promise();
+        redisClient.incr(key, incrementedCount);
+        return incrementedCount.future().map(count -> {
+            if (count == 1) {
+                redisClient.expire(key, LIMITATION_TIME, __ -> {
+                });
+            }
+            if (count > maxRequests) {
+                throw new QueryLimitReachedException(error);
+            }
+            return true;
+        });
+    }
+}
---
+++ src/main/resources/webroot/getUserVideos/script.js
@@ -0,0 +1,91 @@
+const users = [];
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.users');
+
+function formatDate(x) {
+  let date = new Date(x);
+  return date.toLocaleDateString();
+}
+
+function displayUsersAnd(isLoading, errorMsg) {
+  let html = users.map(user => {
+    const userId = user._id;
+    const userPhone = `<span class="hl">${user.phoneNumber}</span>`;
+    return `
+      <li>
+        <span class="name">${userPhone} ${userId}</span>
+        <span class="date">${formatDate(user.createdAt)}</span>
+      </li>
+    `;
+  }).join('')
+    +
+    `<li>
+    <span class="name">Phone number, User id</span>
+    <span class="date">Date of creation</span>
+  </li>`;
+  if (errorMsg) {
+    html = `
+    <li>
+      <span class="name">Error: ${errorMsg}</span>
+    </li>
+  ` + html;
+  }
+  if (isLoading) {
+    html = `
+    <li>
+      <span class="name">Loading...</span>
+    </li>
+  ` + html;
+  }
+  suggestions.innerHTML = html;
+}
+
+function displayUsersWithError(errorMsg) {
+  displayUsersAnd(false, errorMsg);
+}
+
+function displayError(error) {
+  console.log(error);
+  displayUsersWithError(error.message);
+}
+
+function displayUsers() {
+  displayUsersAnd(false, null)
+}
+function displayLoading() {
+  displayUsersAnd(true, null)
+}
+
+function addUser(user, users) {
+  console.log(`Fetching user: ${user}`)
+  fetch(`/users/${user}/videos`)
+    .then(response => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      } else {
+        return response;
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(`User arrived: ${data}`);
+      users.unshift(data);
+      displayUsers();
+    })
+    .catch(displayError)
+    .finally(() => {
+      console.log(`Unblocking the input field`)
+      searchInput.disabled = false;
+    });
+}
+
+searchInput.onkeypress = function (e) {
+  if (!e) e = window.event;
+  var keyCode = e.keyCode || e.which;
+  if (keyCode === 13) {
+    searchInput.disabled = true;
+    displayLoading();
+    addUser(this.value, users);
+  }
+}
